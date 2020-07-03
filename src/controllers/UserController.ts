a7fff34a8f16efd3a92d926982beeb97ad15a098
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-03 18:38:02 -0300

    store user controller added

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -12,6 +12,18 @@ class UserController {
     const {
       name, email, username, city, age,
     } = req.body;
+
+    let user = await knex('users').where({ email });
+    if (user) { return res.status(201).json(user); }
+
+    const possibleUsername = knex('users').where({ username }).select('username');
+    if (possibleUsername) return res.status(400).json({ error: `Já existe o username ${possibleUsername}.` });
+
+    user = await knex('users').insert({
+      name, email, username, city, age,
+    });
+
+    return res.status(200).json(user);
   }
 }
 
