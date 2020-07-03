49dedd8c895e661c0ce2df0ef2ea2becda66165d
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-03 15:27:16 -0300

    users migrations created

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  development: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+    },
+    migrations: {
+      directory: './src/database/migrations',
+    },
+  }
+};
--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,4 @@
 import knex from 'knex';
-import knexConfig from '@config/database';
+import knexConfig from '../../knexfile';
 
 module.exports = knex(knexConfig);
