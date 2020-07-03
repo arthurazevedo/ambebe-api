f2be3f748ee3c59376b57c6d0e5d0d946ef21b3d
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-03 20:53:20 -0300

    jwtMiddleware added in routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 
+import authMiddleware from './middlewares/auth';
+
 import UserController from './controllers/UserController';
 
 const routes = express.Router();
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
