8d08ab914a6c7398dd1c928237ed885921b80645
arthurazevedo <arthur.stevam.costa@ccc.ufcg.edu.br>
2020-07-04 19:32:50 -0300

    socketio testando

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,11 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 io.on('connection', (socket) => {
-  console.log('cliente conectado');
-  io.emit('tudobom', 'fala davi');
-});
-
-io.on('olamundo', (socket) => {
   console.log(socket);
+  io.emit('tudobom', 'fala davi');
+  io.on('olamundo', (data) => {
+    console.log(data);
+  });
 });
 
 server.listen(process.env.PORT || 3333);
