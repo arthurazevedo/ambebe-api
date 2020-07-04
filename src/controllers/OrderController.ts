4d6691f00f047c32c3ca00be64828d6490b62266
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-04 18:25:21 -0300

    create order method improved

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -1,18 +1,18 @@
-import { Request, Response } from 'express';
+import { Request, Response, ErrorRequestHandler } from 'express';
 import knex from '../database';
 
 class OrderController {
   static async create(req: Request, res: Response) {
     const { orders } = req.body;
 
-    if (!orders) return res.status(400).json({ error: 'Informe os valores necessários (id_checkin, id_product, quantity)' });
+    if (!orders) return res.status(400).json({ error: 'A requisição deve conter um array de pedidos.' });
 
     try {
       await knex('orders').insert(orders);
 
       return res.status(200).json({ message: 'Item cadastrado com sucesso.' });
     } catch (err) {
-      return res.status(500).json({ message: err.message });
+      return res.status(500).json({ error: err.message });
     }
   }
 }
